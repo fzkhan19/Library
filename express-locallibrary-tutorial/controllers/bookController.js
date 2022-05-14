d2d5361f139f6225317b319bb2aaa5ea2eb89f9a
Faiz Khan <faizpathan1717@gmail.com>
2022-05-14 19:27:17 +0530

    commit

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -1,11 +1,36 @@
 var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
+
+var async = require('async');
 
 exports.index = (req,res)=>{
-    res.send('Not Implemented: book Index');
+    async.parallel({
+        book_count: (callback)=>{
+            Book.countDocuments({},callback);
+        },
+        book_instance_count: (callback)=>{
+            BookInstance.countDocuments({}, callback);
+        },
+        book_instance_available_count: (callback)=>{
+            BookInstance.countDocuments({status:'Available'}, callback);
+        },
+        author_count: (callback)=>{
+            Author.countDocuments({},callback);
+        },
+        genre_count: (callback)=>{
+            Genre.countDocuments({},callback);
+        }},(err,result)=>{
+        res.render('index',{title: 'Local Library Home', error:err, data:result});
+    });
 }
 
-exports.book_list = (req,res)=>{
-    res.send('Not Implemented: book List');
+exports.book_list = (req,res,next)=>{
+    Book.find({},'title author').sort({title : 1}).populate('author').exec((err,list_books)=>{
+        if(err){return next(err);}
+        res.render('book_list',{title:'Book List'});
+    })
 };
 
 
