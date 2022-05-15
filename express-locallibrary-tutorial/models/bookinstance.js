e4cb79c778fcea849d40b5837f3d65bb09394a8b
Faiz Khan <faizpathan1717@gmail.com>
2022-05-15 12:34:01 +0530

    commit

--- express-locallibrary-tutorial/models/bookinstance.js
+++ express-locallibrary-tutorial/models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
 
 var Schema = mongoose.Schema;
 
@@ -13,6 +14,12 @@ var BookInstanceSchema = new Schema(
 
 BookInstanceSchema.virtual('url').get(()=>{
     return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model('BookInstance',BookInstanceSchema);
