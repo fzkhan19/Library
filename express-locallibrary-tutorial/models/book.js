ca6278b43d5e4483b022241f8f81dac4f9ac9c29
Faiz Khan <faizpathan1717@gmail.com>
2022-05-15 12:03:47 +0530

    commit

--- express-locallibrary-tutorial/models/book.js
+++ express-locallibrary-tutorial/models/book.js
@@ -3,17 +3,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema(
-    {
-        title: {type: String, required: true},
-        summary: {type: String, required: true},
-        isbn: {type: String, required: true},
-        author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
-        genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
-    }
+  {
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
+    summary: {type: String, required: true},
+    isbn: {type: String, required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+  }
 );
 
-BookSchema.virtual('url').get(()=>{
-    return '/catalog/book/' + this._id;
+// Virtual for book's URL
+BookSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/book/' + this._id;
 });
 
-module.exports = mongoose.model('Book',BookSchema);
+//Export model
+module.exports = mongoose.model('Book', BookSchema);
