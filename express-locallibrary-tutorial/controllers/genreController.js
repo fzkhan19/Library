b06a57a66f22edb5efab0245cd0b199b59098233
Faiz Khan <faizpathan1717@gmail.com>
2022-05-15 15:01:36 +0530

    commit

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -1,8 +1,8 @@
+var async = require('async');
+var Book = require('../models/book');
 var Genre = require('../models/genre');
 
-
-exports.genre_list = (req,res)=>{
-    
+exports.genre_list = function(req,res){
     Genre.find()
       .sort([['name', 'ascending']])
       .exec(function (err, list_genres) {
@@ -10,13 +10,37 @@ exports.genre_list = (req,res)=>{
         //Successful, so render
         res.render('genre_list', { title: 'Genre List', genre_list: list_genres });
       });
-  
+      
 };
 
 
-exports.genre_detail = (req,res)=>{
-    res.send('Not Implementd: genre Detail');
+// Display detail page for a specific Genre.
+exports.genre_detail = function(req, res, next) {
+
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+              .exec(callback);
+        },
+
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre==null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+    });
+
 };
+
 
 
 exports.genre_create_get = (req,res)=>{
--- express-locallibrary-tutorial/models/genre.js
+++ express-locallibrary-tutorial/models/genre.js
@@ -8,7 +8,7 @@ var GenreSchema = new Schema(
     }
 );
 
-GenreSchema.virtual('url').get(()=>{
+GenreSchema.virtual('url').get(function(){
     return '/catalog/genre/' + this._id;
 });
 
